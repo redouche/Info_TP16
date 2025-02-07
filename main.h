086960e90e754eaa128b02565d7aa600a6349bba
reda <itsrazenyt@gmail.com>
2025-02-07 10:22:34 +0100

    added details in main.h

--- main.h
+++ main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 
 void afficherTableau(int* tab, int const taille);
-int* allouerTableauEntiersV1(int const taille);
-void allouerTableauEntiersV2(int** pTab, int const taille);
-void allouerTableauEntiersV3(int** pTab, int* pTaille);
+int* allouerTableauEntiersV1(int const taille); // Fonction la plus facile, mais consomme plus
+void allouerTableauEntiersV2(int** pTab, int const taille); // Meilleure fonction, mais plus compliquée
+void allouerTableauEntiersV3(int** pTab, int* pTaille); // Fonction flexible, mais comporte des risques
 
 #endif //MAIN_H
